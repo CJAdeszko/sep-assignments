572078cff57f7830e5a21ff63e5a136837870ce3
Christopher Adeszko <cjadeszko@gmail.com>
2019-04-22 09:18:37 -0700

    Algorithm Complexity Into: Solutions

--- 02-algorithms-and-complexity/03-complexity-intro/worst_case.rb
+++ 02-algorithms-and-complexity/03-complexity-intro/worst_case.rb
@@ -1,2 +1,17 @@
 def worst_case(n)
+  puts "N: #{n} | Worst Case Iterations: #{n}"
 end
+
+
+=begin
+  N: 1 | Worst Case Iterations: 1
+  N: 2 | Worst Case Iterations: 2
+  N: 3 | Worst Case Iterations: 3
+  N: 4 | Worst Case Iterations: 4
+  N: 5 | Worst Case Iterations: 5
+  N: 6 | Worst Case Iterations: 6
+  N: 7 | Worst Case Iterations: 7
+  N: 8 | Worst Case Iterations: 8
+  N: 9 | Worst Case Iterations: 9
+  N: 10 | Worst Case Iterations: 10
+=end
